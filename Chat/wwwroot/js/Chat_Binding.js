055c9a4374e4059ef43867cfed42be2d19e1e7f7
Ignacio Romero <38145751+Nacho018@users.noreply.github.com>
2021-01-19 19:30:12 -0300

    commit

---
+++ Chat/wwwroot/js/Chat_Binding.js
@@ -0,0 +1,24 @@
+﻿var vm = new Vue({
+    el: "#chat",
+    data: {
+        isHidden: true,
+        btnisHidden: false,
+        Consulta: ""
+    },
+
+    methods: {
+
+        mostrarform: function () {
+
+            this.isHidden = false;
+            this.btnisHidden = true
+
+        },
+        cerrarform: function () {
+
+            this.isHidden = true;
+            this.btnisHidden = false
+
+        },
+    }
+});  
